e90f8f4537c638d3b50e481834118bc0f353dd1a
deano98 <deano_98@hotmail.com>
2021-06-26 14:42:10 +0000

    Add code for division section

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,9 @@ function runGame(gameType) {
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
-        displaySubtractQuestion(num1, num2);    
+        displaySubtractQuestion(num1, num2);  
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1 * num2, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,7 +91,9 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"]; 
     } else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];      
+        return [operand1 - operand2, "subtract"];   
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -136,4 +140,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
 
+}
+
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
